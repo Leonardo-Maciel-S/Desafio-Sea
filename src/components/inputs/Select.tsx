72247aec324ee42c5e651169115cec3688f0a3e6
Leonardo Maciel <leonardomacielj@gmail.com>
2025-01-20 22:49:54 -0300

    fix: Select not close when click out component

--- src/components/inputs/Select.tsx
+++ src/components/inputs/Select.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface Select {
 	defaultValue: string;
@@ -8,6 +8,7 @@ interface Select {
 const Select = ({ defaultValue, options }: Select) => {
 	const [isOptionModalOpen, setIsOptionModalOpen] = useState(false);
 	const defaultOption = useRef("");
+	const selectRef = useRef<HTMLDivElement>(null);
 
 	const option = defaultOption.current || defaultValue;
 
@@ -17,25 +18,42 @@ const Select = ({ defaultValue, options }: Select) => {
 		defaultOption.current = option;
 	};
 
+	const handleClickOutside = (event: MouseEvent) => {
+		if (
+			selectRef.current &&
+			!selectRef.current.contains(event.target as Node)
+		) {
+			setIsOptionModalOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("click", handleClickOutside);
+
+		return () => {
+			document.removeEventListener("click", handleClickOutside);
+		};
+	}, []);
+
 	return (
-		<div className="relative">
+		<div className="relative" ref={selectRef}>
 			<button
 				type="button"
 				onClick={() => setIsOptionModalOpen(!isOptionModalOpen)}
-				className="w-full px-[12px] py-1 outline-none border border-defaultBlue rounded-[10px] text-base font-medium text-dark placeholder:text-dark flex justify-between items-center h-8"
+				className="select w-full px-[12px] py-1 outline-none border border-defaultBlue rounded-[10px] text-base font-medium text-dark placeholder:text-dark flex justify-between items-center h-8"
 			>
 				<span>{option}</span>
 				<div className="w-2 h-2 border-b-2 border-s-2 border-black -rotate-[45deg] mr-1 mb-1 " />
 			</button>
 
 			{isOptionModalOpen && (
-				<div className="flex flex-col w-full absolute border border-black items-start bg-white">
+				<div className="flex flex-col w-full absolute border bg-white border-black items-start z-20 ">
 					{options?.map((option) => (
 						<button
 							type="button"
 							onClick={() => handleClick(option)}
 							key={option}
-							className="hover:bg-zinc-200 w-full px-[12px] py-1 flex "
+							className="hover:bg-zinc-200 w-full px-[12px] py-1 flex z-30 "
 						>
 							{option}
 						</button>
