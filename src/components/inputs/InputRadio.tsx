d605cf79ac5cb41d85a9e7b716850fd96e2f6958
Leonardo Maciel <leonardomacielj@gmail.com>
2025-01-22 23:28:35 -0300

    fix: bug gender error not resolve

--- src/components/inputs/InputRadio.tsx
+++ src/components/inputs/InputRadio.tsx
@@ -1,4 +1,5 @@
-import { type HTMLAttributes, useState } from "react";
+import { type HTMLAttributes, useContext, useState } from "react";
+import { NewEmployeeContext } from "../../context/NewEmployeeContext";
 
 interface InputRadioProps extends HTMLAttributes<HTMLInputElement> {
 	setInputGender: React.Dispatch<React.SetStateAction<string>>;
@@ -18,28 +19,45 @@ const InputRadio = ({ setInputGender }: InputRadioProps) => {
 		setInputGender("masculino");
 	};
 
+	const context = useContext(NewEmployeeContext);
+	if (!context) return;
+	const {
+		register,
+		formState: { errors },
+	} = context.formMethods;
+
 	return (
-		<div className="flex gap-2 items-center h-8">
-			<button
-				type="button"
-				onClick={() => handleClick("female")}
-				className="flex items-center justify-center gap-2 "
-			>
-				<div
-					className={`${selected === "female" && "bg-default"} size-4 border border-bgRadio rounded-full transition-all delay-75 linear`}
-				/>
-				<span>Feminino</span>
+		<div className="flex gap-2 items-center h-8 ">
+			<button type="button" onClick={() => handleClick("female")} className="">
+				<label
+					htmlFor="female"
+					className="flex items-center justify-center gap-2"
+				>
+					<div
+						className={`${selected === "female" && "bg-default"} size-4 border border-bgRadio rounded-full transition-all delay-75 linear`}
+					/>
+					<span>Feminino</span>
+
+					<input
+						type="radio"
+						id="female"
+						{...register("gender")}
+						value="female"
+					/>
+				</label>
 			</button>
 
-			<button
-				type="button"
-				onClick={() => handleClick("male")}
-				className="flex items-center justify-center gap-2 "
-			>
-				<div
-					className={`${selected === "male" && "bg-default"} size-4 border border-bgRadio rounded-full transition-all delay-75 linear`}
-				/>
-				<span>Masculino</span>
+			<button type="button" onClick={() => handleClick("male")}>
+				<label
+					htmlFor="male"
+					className="flex items-center justify-center gap-2 "
+				>
+					<div
+						className={`${selected === "male" && "bg-default"} size-4 border border-bgRadio rounded-full transition-all delay-75 linear`}
+					/>
+					<span>Masculino</span>
+					<input type="radio" id="male" {...register("gender")} value="male" />
+				</label>
 			</button>
 		</div>
 	);
