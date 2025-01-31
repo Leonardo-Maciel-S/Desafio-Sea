d7c09ba242b47d75cbde1e4e7f0c86baf3b33858
Leonardo Maciel <leonardomacielj@gmail.com>
2025-01-31 09:25:16 -0300

    fix: margin in spaceBetweenCards

--- src/components/stages/SpaceBetweenCards.tsx
+++ src/components/stages/SpaceBetweenCards.tsx
@@ -1,7 +1,7 @@
 const SpaceBetweenCards = ({ className }: { className?: string }) => {
 	return (
 		<div
-			className={`hidden phone:flex size-1 mb-[55px] border-t-4 border-defaultBlue border-dashed flex-1 min-w-8  ${className}`}
+			className={`hidden phone:flex size-1 mb-[70px] border-t-4 border-defaultBlue border-dashed flex-1 min-w-8  ${className}`}
 		/>
 	);
 };
